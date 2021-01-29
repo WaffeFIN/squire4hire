e252ce4b7138517c4b1fa071d58920e4b7dc78e0
EchoesOfNorth <vuorpa13@gmail.com>
2021-01-30 00:43:58 +0200

    Added audio manager

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+    public bool loop;
+
+    public AudioSource AddToSource(AudioSource source) {
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.loop = loop;
+        return source;
+    }
+}
