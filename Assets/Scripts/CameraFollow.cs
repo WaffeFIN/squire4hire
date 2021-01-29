76e34126a339e880bd7274030e7640b85b5e06e2
EchoesOfNorth <vuorpa13@gmail.com>
2021-01-29 22:04:27 +0200

    Trying to get the camera to follow hero

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform heroPosition;
+    public Transform HeroTransform;
 
-    void FixedUpdate() {
-        gameObject.transform.position = new Vector3(heroPosition.position.x, heroPosition.position.y);
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    public float CameraDistance;
+    void Awake() {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / CameraDistance);
+    }
+
+    void LateUpdate() {
+        transform.position = Vector3.Slerp(transform.position, HeroTransform.position, SmoothFactor);
     }
 }
