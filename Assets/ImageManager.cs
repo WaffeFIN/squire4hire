1ccdb8aa0878e5dd127cae6eacb331c8258c46e0
Walter Grönholm <waltergronholm@gmail.com>
2021-01-29 21:03:39 +0200

    added movement to player

---
+++ Assets/ImageManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageManager : MonoBehaviour
+{
+	public Image image;
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.transform.position = gameObject.transform.position;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+		var rigidBody = GetComponent<Rigidbody2D>();
+		var vel = rigidBody.velocity;
+		var dx = 0;
+		var dy = 0;
+		var acc = 22;
+        if (Input.GetKey("up")) {
+			dy += acc;
+		}
+        if (Input.GetKey("down")) {
+			dy -= acc;
+		}
+        if (Input.GetKey("right")) {
+			dx += acc;
+		}
+        if (Input.GetKey("left")) {
+			dx -= acc;
+		}
+		rigidBody.velocity = new Vector2(vel.x + dx, vel.y + dy);
+		var maxSpeed = 220.0f;
+		if (rigidBody.velocity.magnitude > maxSpeed) {
+			rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
+		}
+    }
+}
