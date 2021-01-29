185c968f6f343ec2f8a2c35dcf39a96ecef60fff
EchoesOfNorth <vuorpa13@gmail.com>
2021-01-29 20:42:32 +0200

    Add basic hero movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform heroPosition;
+
+    void FixedUpdate() {
+        gameObject.transform.position = new Vector3(heroPosition.position.x, heroPosition.position.y);
+    }
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -10,12 +10,15 @@ public class Hero : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.gameObject.transform.position = new Vector3(200, 200);
+        image.transform.position = new Vector3(200, 200);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var currentPosition = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(currentPosition.x + 1, currentPosition.y);
+        image.transform.position = new Vector3(currentPosition.x + 1, currentPosition.y);
     }
 }
