54319e7255682a8e787b9ce889533f309bb0c6b7
EchoesOfNorth <vuorpa13@gmail.com>
2021-01-30 19:30:54 +0200

    Added proper overlay and inventory as text

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public Camera currentCamera;
+    public Inventory playerInventory;
+
+    // Overlay visuals
+    public GameObject overlay;
     public GameObject pauseMenu;
+    public GameObject inventoryContent;
 
     private bool isGameOngoing = true;
 
@@ -18,14 +24,29 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var currentCameraPosition = currentCamera.transform.position;
+        overlay.transform.position = new Vector2(currentCameraPosition.x, currentCameraPosition.y);
+
         if (Input.GetKeyDown(KeyCode.P)) {
             isGameOngoing = !isGameOngoing;
             Time.timeScale = isGameOngoing ? 1 : 0;
-            if (!isGameOngoing) {
-                var currentCameraPosition = currentCamera.transform.position;
-                pauseMenu.transform.position = new Vector2(currentCameraPosition.x, currentCameraPosition.y);
-            }
             pauseMenu.SetActive(!isGameOngoing);
+        }
+
+        if (playerInventory.itemsCarried.Count > 0) {
+            var content = "";
+            for (int i = 0; i < playerInventory.itemsCarried.Count; i++)
+            {
+                var itemName = playerInventory.itemsCarried[i].GetComponent<Item>().GetName();
+                content += itemName;
+                if (i < playerInventory.itemsCarried.Count - 1) {
+                    content += ", ";
+                }
+            }
+           
+            inventoryContent.GetComponent<Text>().text = content;
+            
+            inventoryContent.GetComponent<Text>().color = playerInventory.IsFull() ? new Color(1f, 0.1f, 0.1f) : new Color(1f, 1f, 1f);
         }
     }
 }
