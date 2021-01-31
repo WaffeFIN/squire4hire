feec1abfb5c5c7d35aee3703013f5b3678d6cab9
Walter Grönholm <waltergronholm@gmail.com>
2021-01-31 12:24:45 +0200

    forgot blood files

---
+++ Assets/Scripts/Blood.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour
+{
+	public float decay;
+	public Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        decay = Random.Range(0.6f, 1.4f);
+		decay *= decay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = new Vector3(transform.position.x + velocity.x, transform.position.y + velocity.y, transform.position.z);
+
+		var friction = 12.0f;
+        if (velocity.magnitude > 0) {
+            velocity -= friction * velocity.normalized * Time.deltaTime;
+            if (velocity.magnitude < friction) {
+                velocity = new Vector2();
+            }
+        }
+
+        decay -= Time.deltaTime;
+		if (decay < 0)
+			Destroy(gameObject);
+    }
+}
